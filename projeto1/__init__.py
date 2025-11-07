83d0f81bda828b1b579209b0adc7e9803087d6f6
franciscoedd <eduardo-francisco@hotmail.com>
2025-11-07 15:28:00 -0300

    Update __init__.py

--- projeto1/__init__.py
+++ projeto1/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'd0e4039ea80646893676aa924dcb0a6c'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projeto.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
